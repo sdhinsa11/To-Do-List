f6b15504205333da44828329f91e2596fcbc8416
SDHINSA <sohanad@icloud.com>
2024-07-18 12:39:09 -0600

    plan it out

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 alert("this is start of to do list");
+
+// need a class module for the actual to do list 
+// need a page for the default where they see all the to-do lists 
+// need a file for the different tasks 
+// notes tab 
+
+// the above will keep the tabs constant but what is displayed will change so we need different files for that
+// THIS IS combining the restaurant project, the library and the organization of tic tac toe
