b2c406ab2b4db03240921ba347c08eb621dd2024
SDHINSA <sohanad@icloud.com>
2024-07-22 17:59:50 -0600

    put it all in steps

--- src/index.js
+++ src/index.js
@@ -23,3 +23,8 @@ function Task(title, description, date, section){
 //first get the default page done nicely then you'll be able to add things for the other pages
 // each to do should have title, desc, notes, date and what section its in and a delete button
 // show it as the title first and date then when you click on it it should expand
+
+
+// step 1: work on showing the to dos on the default page (create them , add them, organize by date)
+// step 2: be able to expand them 
+// step 3: start the next pages 
