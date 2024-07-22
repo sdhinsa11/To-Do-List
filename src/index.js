2a1a0ec0c7e0a50b415cfdcaed02f9637f08a6a0
SDHINSA <sohanad@icloud.com>
2024-07-22 17:58:43 -0600

    put it all in steps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-alert("this is start of to do list");
+
+//use factory for dynamically creating the to do list
 
 // need a class module for the actual to do list 
 // need a page for the default where they see all the to-do lists 
@@ -7,3 +8,18 @@ alert("this is start of to do list");
 
 // the above will keep the tabs constant but what is displayed will change so we need different files for that
 // THIS IS combining the restaurant project, the library and the organization of tic tac toe
+
+
+function Task(title, description, date, section){
+
+
+}
+
+
+// 3 sections
+// work, health, personal 
+// create a to do when click the plus button (like in library)
+// have a list of the object created, and list for the other three, put them in there pages and yeah, write the code for the other pages in there but clal it in here 
+//first get the default page done nicely then you'll be able to add things for the other pages
+// each to do should have title, desc, notes, date and what section its in and a delete button
+// show it as the title first and date then when you click on it it should expand
