d05ba1f258a1c85c87b2bc48a44f6c699805f08a
SDHINSA <sohanad@icloud.com>
2024-07-23 21:02:25 -0600

    Finished the basics of the project

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ function main(){
 
 main();
 
+// have an add button to add the tasks - this should not move 
+// filtering logic 
+// switch pages  
+
+
+
 
 // now we need to filter based on the page and so we grab the tasks and we add them 
 // this is where the dom comes in 
